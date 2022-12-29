5b5de7f74a0a81a2cb82a13abcf66554d889a5ec
CaioVitor1 <caiovitor33@hotmail.com>
2022-12-29 17:55:43 -0300

    feat: add eslint configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRouter } from "./routes";
-import { AppThemeProvider } from "./shared/contexts";
+import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './routes';
+import { AppThemeProvider } from './shared/contexts';
 export const App = () => {
-  return (
-    <AppThemeProvider>
-      <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
-    </AppThemeProvider>
+    return (
+        <AppThemeProvider>
+            <BrowserRouter>
+                <AppRouter />
+            </BrowserRouter>
+        </AppThemeProvider>
     
-  );
-}
+    );
+};
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@mui/material";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts";
-
+import { Button } from '@mui/material';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
+import React from 'react';
 export const AppRouter = () => {
-    const { toggleTheme } = useAppThemeContext()
+    const { toggleTheme } = useAppThemeContext();
 
     return (
-    <Routes>
-        <Route path="/initialPage" element={<Button variant='contained' color='primary' onClick={toggleTheme} >página inicial </Button>} />
-        <Route path="*" element={<Navigate to={"/initialPage"} />} />
-    </Routes>)
-}
+        <Routes>
+            <Route path="/initialPage" element={<Button variant='contained' color='primary' onClick={toggleTheme} >página inicial </Button>} />
+            <Route path="*" element={<Navigate to={'/initialPage'} />} />
+        </Routes>);
+};
